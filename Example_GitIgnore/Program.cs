62a5909ca4a85b397cdc139fe9bf6fd42d02b27f
FratNightGaming <jberger1@iwu.edu>
2022-10-13 12:22:37 -0500

    added everything else

---
+++ Example_GitIgnore/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Example_GitIgnore
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is an example of using a git ignore!");
+        }
+    }
+}
